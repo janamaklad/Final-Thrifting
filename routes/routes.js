a1b45465fc25ba6cf410c479ca0b405ec57b1e0c
janamaklad <jana2200074@miuegypt.edu.eg>
2024-06-20 23:42:35 +0300

    ejs

---
+++ routes/routes.js
@@ -0,0 +1,11 @@
+function setupRoutes(app){
+    app.get('/', (req, res) => {
+        res.render('aboutus', { title: 'About Us' });
+    });
+    app.get('/addOrder', (req, res) => {
+        res.render('addOrder', { title: 'Add Order' });
+    });
+}
+module.exports={
+    setupRoutes
+};
