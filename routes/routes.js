3f064742379406b5f8f183be5a4f5b82f60fd0cf
janamaklad <jana2200074@miuegypt.edu.eg>
2024-06-21 00:12:23 +0300

    routes

--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,16 @@ function setupRoutes(app){
     app.get('/addOrder', (req, res) => {
         res.render('addOrder', { title: 'Add Order' });
     });
+    app.get('/adminhomeproducts', (req, res) => {
+        res.render('admin home products', { title: 'Home Products' });
+    });
+    app.get('/admin', (req, res) => {
+        res.render('admin', { title: 'Second Chance' });
+    });
+    app.get('/AdminBaby', (req, res) => {
+        res.render('AdminBaby', { title: 'Baby Category' });
+    });
+    
 }
 module.exports={
     setupRoutes
