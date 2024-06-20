69a522b6c25224af02c02f08326710279b114840
janamaklad <jana2200074@miuegypt.edu.eg>
2024-06-21 02:22:32 +0300

    .js

--- App.js
+++ App.js
@@ -1,22 +1,35 @@
-const {setupRoutes}= require("./routes/routes")
 const express = require("express");
-const path = require('path');
+const path = require("path");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const routes = require("./routes/routes"); // Import your routes setup function
+
 const app = express();
+
 app.use(express.static("public", { maxAge: "7d" }));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname,'public')));
+require('dotenv').config();
 
 
-setupRoutes(app);
+// Session configuration
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
 
+// Setup routes
+routes.setupRoutes(app); // Call setupRoutes and pass app as an argument
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
-
--- models/userModel.js
+++ models/userModel.js
@@ -2,10 +2,27 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
   username: {
     type: String,
     required: true,
     unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
   },
   password: {
     type: String,
