b8c4c6b14aab1306b2a1afcea62edfb10944ff86
samahegazy22 <sama2204043@miuegypt.edu.eg>
2024-06-22 02:13:27 +0300

    app

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const routes = require("./routes/routes"); // Assuming this is your routes file
 const app = express();
 
-const productRoutes = require('./routes/productRoutes');
-
+// Middleware setup
 app.use(express.static("public", { maxAge: "7d" }));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-<<<<<<< HEAD
 require('dotenv').config();
 
   // Session setup
@@ -25,14 +29,26 @@ require('dotenv').config();
   next(); // Pass control to the next middleware function
 });
 
-// Setup routes 
+// Setup routes using your routes module
 routes.setupRoutes(app);
-=======
-app.use('./controllers/products', productRoutes);
->>>>>>> ea6a96a3a4f585cc137640f4eeb7b9783c899154
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
-    console.log('Server running on http://localhost:${PORT}');
+  console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
+ /*app.post("/register", (req,res)=>{
+  try{
+  const { username, password }= req.body;
+  const findUser = user.find((data) => email == data.email);
+  if (findUser){
+  res.status(400).send("Wrong email or password");
+  }
+  const hashedPassword = await bcrypt.hash(password,10);
+  user.push({username, password: hashedPassword });
+  res.status(201).send("success");
+  } catch (err){
+  res.status(500).send({message: err.message})
+  }
+  })*/
