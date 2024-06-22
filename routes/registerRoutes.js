dba072a65e222e140ccc71a367dcf935b1baaab2
JomanaAyman <jomana27904@gmail.com>
2024-06-22 19:17:04 +0300

    file organization and rename

--- routes/registerRoutes.js
+++
@@ -1,8 +0,0 @@
-const signupControllers = require("../controllers/signupControllers");
-const express = require("express");
-const app=express();
-const bodyParser = require("body-parser");
-
-app.post("/signup", signupControllers.registrationProcess);
-
-module.exports=app;
