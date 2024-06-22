5c836e78a52d52d1ea676f7359219c6dca422add
JomanaAyman <jomana27904@gmail.com>
2024-06-22 22:22:29 +0300

    fixed errors and added changes in codes

--- models/user.js
+++ models/user.js
@@ -1,35 +1,50 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
-  fname: {
+  role: {
     type: String,
+    enum: ["user", "admin"],
+    default: "user",
     required: true,
   },
-  lname: {
+  firstName: {
     type: String,
-    required: true,
+    required: [true, "First name is required"],
+  },
+  lastName: {
+    type: String,
+    required: [true, "Last name is required"],
   },
   username: {
     type: String,
-    required: true,
+    required: [true, "Username is required"],
     unique: true,
   },
   email: {
     type: String,
-    required: true,
+    required: [true, "Email is required"],
     unique: true,
+    match: [/.+\@.+\..+/, "Please enter a valid email address"],
   },
-  num: {
+  phoneNumber: {
     type: String,
-    required: true,
+    required: [true, "Phone number is required"],
+    match: [/^\d{11}$/, "Please enter a valid 10-digit phone number"],
   },
   password: {
     type: String,
-    required: true,
+    required: [true, "Password is required"],
+    minlength: [6, "Password must be at least 6 characters long"],
   },
+  products: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+    },
+  ],
 });
 
-const User = mongoose.model("User", userSchema);
+const User = model("User", userSchema);
 
-module.exports = User;
+export default User;
