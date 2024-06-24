616bcde44956a90c49e30defdb01839b0839d985
malakelmahdy <malakelmahdy81@gmail.com>
2024-06-24 07:46:14 +0300

    user controller

--- controllers/user.js
+++ controllers/user.js
@@ -36,7 +36,7 @@ const home_get = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     if (!user) return res.status(404).json({ message: "User not found" });
-    res.render("index", {
+    res.render("user/shop", {
       title: "shop",
       user: await User.findById(req.params.id),
     });
@@ -151,7 +151,6 @@ const deleteUser_delete = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
-
 
 export {
   createUser_post,
